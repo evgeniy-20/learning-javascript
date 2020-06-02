b216625e23281fc052edc7f783216a4866bd5fee
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-06-02 15:42:56 +0300

    made an object for data entry

--- passport-data.js
+++ passport-data.js
@@ -1,7 +1,10 @@
-function makeUserObject (name,surname,age) {
-    this.name = name;
-    this.surname = surname;
-    this.age = age
+const makeUserObject = (name,surname,age) => {
+    const result = {
+        name: name,
+        surname: surname,
+        age: age
+    }
+    return result
 }
 const makeUserObject1 = new makeUserObject('Evgeniy', 'Prokopiv', 22);
 console.log(makeUserObject1);
