94fa45b13f7a6ad7f59aae922c4894311fc162fe
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-06-03 20:34:50 +0300

    button

---
+++ input.js
@@ -0,0 +1,11 @@
+const TODO_ELEMENTS = {
+    INPUT: 'input',
+    SUBMIT: 'submit',
+    TODOLIST: 'todoList'
+}
+const input = document.getElementById(TODO_ELEMENTS.INPUT);
+const submit = document.getElementById(TODO_ELEMENTS.SUBMIT);
+const todoList = document.getElementById(TODO_ELEMENTS.TODOLIST);
+console.log(input);
+console.log(submit);
+console.log(todoList);
