a5a9a8eecd8fb47029628053ff337316c15b9682
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-06-10 17:11:09 +0300

    task-3

---
+++ task-2/input.js
@@ -0,0 +1,5 @@
+const greetByName = () => {
+    const nameInput = document.getElementById("name")
+    const text = nameInput.value + " Привет";
+    alert(text);
+}
---
+++ task-3/app.js
@@ -0,0 +1,6 @@
+const greetByName = () => {
+    const nameInput = document.getElementById("name")
+    const elementInput = document.getElementById("element")
+    const text = nameInput.value + " Привет";
+    element.innerHTML = text;
+}
