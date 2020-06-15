b018ee7757642764006146a038204cffec40f466
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-06-15 14:55:27 +0300

    corrected the mistake

--- task-5/app.js
+++ task-5/app.js
@@ -1,17 +1,9 @@
-const greetByName = () => {
-    const nameInput = document.getElementById("name")
-    const numberInput = document.getElementById("number")
-    const elementInput = document.getElementById("element")
-    const sum = calculateSum;
-    element.innerHTML = sum;
-    elementInput.innerHTML = sum;
-    numberInput.innerHTML = sum;
-}
-const calculateSum = (nameInput, numberInput) => {
+const calculateSum = () => {
     const numElement1 = document.getElementById("number1");
     const numElement2 = document.getElementById("number2");
+    const resultElement = document.getElementById("result");
     const numberValue1 = numElement1.value;
     const numberValue2 = numElement2.value;
-    const sum = numberValue1 + numberValue2; 
-    result.innerHTML = sum;
+    const sum = Number(numberValue1) + Number(numberValue2); 
+    resultElement.innerHTML = sum;
 }
