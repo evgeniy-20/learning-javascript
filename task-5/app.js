ca56179c034796419c89cd7b84e2c2bfd0983c48
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-06-15 14:18:15 +0300

    task5

--- task-5/app.js
+++ task-5/app.js
@@ -2,8 +2,16 @@ const greetByName = () => {
     const nameInput = document.getElementById("name")
     const numberInput = document.getElementById("number")
     const elementInput = document.getElementById("element")
-    const sum = nameInput.value + numberInput.value;
+    const sum = calculateSum;
     element.innerHTML = sum;
     elementInput.innerHTML = sum;
     numberInput.innerHTML = sum;
+}
+const calculateSum = (nameInput, numberInput) => {
+    const numElement1 = document.getElementById("number1");
+    const numElement2 = document.getElementById("number2");
+    const numberValue1 = numElement1.value;
+    const numberValue2 = numElement2.value;
+    const sum = numberValue1 + numberValue2; 
+    result.innerHTML = sum;
 }
