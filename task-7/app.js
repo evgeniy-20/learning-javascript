9cf03e33b1eb864e2006642f85726e7d1fe6259d
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-06-20 20:33:29 +0300

    task-7

--- task-7/app.js
+++ task-7/app.js
@@ -1,5 +1,5 @@
 const generationRandomNumber = () => {
     const scrietsInput = document.getElementById('scriets');
     const number = Math.floor(Math.random() * 9);
-    number.innerHTML = scrietsInput;
+    scrietsInput.innerHTML = number;
 }
