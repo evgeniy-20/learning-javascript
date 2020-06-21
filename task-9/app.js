f794e3348b56eae447bcf6cb17b33bcec374d97d
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-06-21 19:44:12 +0300

    task-9

---
+++ task-9/app.js
@@ -0,0 +1,17 @@
+const getRandomNumber = () => {
+    const number = Math.floor(Math.random() * 9);
+    return number;
+}
+const getRandomSecret = () => {
+    const arrayOfNumbers = [];
+    for(let i = 0; i <= 3; i++) {
+        const randomNumber = getRandomNumber();
+        arrayOfNumbers.push(randomNumber);
+    }
+    return arrayOfNumbers;
+}
+const displaySecret = () => {
+    const secretInput = document.getElementById('secret');
+    const numbers = getRandomSecret();
+    secretInput.innerHTML = numbers;
+}
