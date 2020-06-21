4ad1edc429e0df3227a0125b4c5e05f02aa8d42f
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-06-21 13:35:42 +0300

    task-8

---
+++ task-8/app.js
@@ -0,0 +1,9 @@
+const getRandomNumber = () => {
+    const number = Math.floor(Math.random() * 9);
+    return number;
+}
+getRandomNumber();
+const getRandomSecret = () => {
+    getRandomNumber
+}
+console.log(getRandomSecret);
