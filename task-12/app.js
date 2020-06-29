f4eff3e78ca945e7f2df70874207f44970cf9182
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-06-29 22:55:53 +0300

    task-12

---
+++ task-12/app.js
@@ -0,0 +1,40 @@
+const getRandomNumber = () => {
+    const number = Math.floor(Math.random() * 9);
+    return number;
+}
+const getRandomSecret = () => {
+    const arrayOfNumbers = [];
+    while(arrayOfNumbers.length < 4) {
+        const randomNumber = getRandomNumber();
+        const result = arrayOfNumbers.includes(randomNumber);
+        if(result === false) {
+            arrayOfNumbers.push(randomNumber);
+        }
+    }
+    return arrayOfNumbers;
+}
+const displaySecret = () => {
+    const secretInput = document.getElementById('secret');
+    const numbers = getRandomSecret();
+    secretInput.innerHTML = numbers;
+}
+const сheckSecret = () => {
+    const numElement1 = document.getElementById("number1");
+    const resultList = document.getElementById("result")
+    const numberValue1 = numElement1.value 
+    if(numberValue1.length != 4) {
+        alert("Пожалуйста введите 4 цифры")
+    } else {
+        const node = document.createElement("li"); 
+        const sumOfNumbers = calcSum(numberValue1);
+        node.innerHTML = numberValue1 + ' ' + sumOfNumbers;
+        resultList.appendChild(node);
+    }
+}
+const calcSum = (numberValue1) => {
+    let sum = 0;
+    for (let i = 0; i < numberValue1.length; i++) {
+        sum = sum + Number(numberValue1[i]);
+    }
+    return sum;
+}
