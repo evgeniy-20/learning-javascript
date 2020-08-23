d48ecb7170689fd908d431d3097ed0e020aa3b4b
evgeniy20 <evgeniy.prokopiv@gmail.com>
2020-08-23 18:31:29 +0300

    task-15

--- task-15/app.js
+++ task-15/app.js
@@ -77,29 +77,224 @@
 // const arr2 = arr[2] * arr[3];
 // const result = arr1 + arr2;
 // console.log(result);
-const obj = {a: 1, b: 2, c: 3};
-const a = obj["c"];
-console.log(a);
-const obj1 = {a: 1, b: 2, c: 3};
-const b = obj1.c;
-console.log(b);
-const obj2 = {Коля: '1000', Вася: '500', Петя: '200'};
-const c = obj2.Коля;
-const v = obj2.Петя;
-console.log(c,v);
-const obj3 = {1: 'пн', 2: 'вт', 3: 'ср', 4: 'чт', 5: 'пт', 6: 'сб', 7: 'вс'};
-const day = obj3[6];
-console.log(day);
-const arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
-const g = arr[1][0];
-console.log(g);
-const obj4 = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}
-const n = obj4.js[0];
-console.log(n);
-const arr1 = {
-	'ru':['пн','вт','ср','чт','пт','сб', 'вс'],
-	'en':['mo','tu','we','th','fr','sa', 'su'],
-};
-const f = arr1['ru'][0];
-const k = arr1['en'][2];
-console.log(f,k)
+// const obj = {a: 1, b: 2, c: 3};
+// const a = obj["c"];
+// console.log(a);
+// const obj1 = {a: 1, b: 2, c: 3};
+// const b = obj1.c;
+// console.log(b);
+// const obj2 = {Коля: '1000', Вася: '500', Петя: '200'};
+// const c = obj2.Коля;
+// const v = obj2.Петя;
+// console.log(c,v);
+// const obj3 = {1: 'пн', 2: 'вт', 3: 'ср', 4: 'чт', 5: 'пт', 6: 'сб', 7: 'вс'};
+// const day = obj3[6];
+// console.log(day);
+// const arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+// const g = arr[1][0];
+// console.log(g);
+// const obj4 = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}
+// const n = obj4.js[0];
+// console.log(n);
+// const arr1 = {
+// 	'ru':['пн','вт','ср','чт','пт','сб', 'вс'],
+// 	'en':['mo','tu','we','th','fr','sa', 'su'],
+// };
+// const f = arr1['ru'][0];
+// const k = arr1['en'][2];
+// console.log(f,k)
+// Условые 
+// const a = 0;
+// if(a === 0) {
+//     console.log('Верно')
+// } else {
+//     console.log("Неверно")
+// }
+// const a = 1;
+// if(a > 0) {
+//     console.log('Верно')
+// } else {
+//     console.log("Неверно")
+// }
+// const a = -3;
+// if(a < 0) {
+//     console.log('Верно')
+// } else {
+//     console.log("Неверно")
+// }
+// const a = 0;
+// if(a >= 0) {
+//     console.log('Верно')
+// } else {
+//     console.log("Неверно")
+// }
+// const a = -3;
+// if(a <= 0) {
+//     console.log('Верно')
+// } else {
+//     console.log("Неверно")
+// }
+// const a = 1;
+// if(a != 0) {
+//     console.log('Верно')
+// } else {
+//     console.log("Неверно")
+// }
+// const a = 'тест';
+// if(a === 'test') {
+//     console.log('Верно')
+// } else {
+//     console.log("Неверно")
+// }
+// const a = '1';
+// if(a === '1') {
+//     console.log('Верно')
+// } else {
+//     console.log("Неверно")
+// }
+// const a = 2;
+// if(a > 0 && a < 5) {
+//     console.log('Верно')
+// } else {
+//     console.log("Неверно")
+// }
+// const a = 2;
+// if(a === 0 || a === 2) {
+//     console.log(a + 7)
+// } else {
+//     console.log(a / 10)
+// }
+// const a = 3;
+// const b = 5;
+// if(a <= 1 || b >= 3) {
+//     console.log(a + b)
+// } else {
+//     console.log(a - b)
+// }
+// const a = 3;
+// const b = 5;
+// if(a > 2 && a < 11 || b >= 6 && b < 6) {
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// }
+// const num = "1, 2, 3, 4";
+// switch (num) {
+// 	case '1':
+// 		console.log('Зима');
+//  	break;
+// 	case '2': 
+//         console.log('Весна');
+// 	break;
+// 	case '3': 
+//         console.log('Лето');
+// 	break;
+// 	case '4':
+// 		console.log('Осень');
+// 	break;
+// }
+
+// Дана строка из 6-ти цифр. Проверьте, что сумма первых трех цифр равняется сумме вторых трех цифр. Если это так - выведите 'да', в противном случае выведите 'нет'
+const sum = 1 + 2 + 3;
+const sum1 = 4 + 5 + 6;
+if(sum === sum1) {
+    console.log('Yes')
+} else {
+    console.log('No')
+}
+// В переменной day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
+const day = 15;
+if(day >= 1 && day <= 10) {
+    console.log("в первую");
+}
+if(day >= 11 && day <= 20) {
+    console.log("во вторую");
+}
+if(day >= 21 && day <= 31) {
+    console.log("в третью");
+}
+//В переменной month лежит какое-то число из интервала от 1 до 12. Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
+const month = 10;
+if(month >= 0 && month <= 3) {
+    console.log("зима");
+}
+if(month >= 4 && month <= 6) {
+    console.log("весна");
+}
+if(month >= 7 && month <= 9) {
+    console.log("лето");
+}
+if(month >= 10 && month <= 12) {
+    console.log("осень");
+}
+// Дана строка, состоящая из символов, например, 'abcde'. Проверьте, что первым символом этой строки является буква 'a'. Если это так - выведите 'да', в противном случае выведите 'нет'.
+const length = 'abcde';
+if('a' === 0) {
+    console.log("Да");
+} else {
+    console.log("Нет");
+}
+// Выведите столбец чисел от 1 до 100.
+for(let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+// Выведите столбец чисел от 11 до 33.
+for(let i = 11; i <= 33; i++) {
+    console.log(i);
+}
+//Выведите столбец четных чисел в промежутке от 0 до 100.
+for(let i = 0; i <= 100; i++) {
+    console.log(i++);
+}
+//С помощью цикла найдите сумму чисел от 1 до 100.
+let a = 0;
+for(let i = 1; i <= 100; i++) {
+    a += i
+    console.log(a);
+}
+//Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for выведите все эти элементы на экран.
+const numbers = [1,2,3,4,5]
+for(let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+//Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for найдите сумму элементов этого массива. Запишите ее в переменную result.
+let result = 0;
+const arr = [1,2,3,4,5]
+for(let i = 0; i < arr.length; i++) {
+    result += arr[i]
+    console.log(result);
+}
+// Дан объект obj. С помощью цикла for-in выведите на экран ключи и элементы этого объекта.
+const obj = {green: 'зеленый', red: 'красный', blue: 'голубой'}
+for(key in obj) {
+    console.log(key);
+}
+//Дан массив с элементами 2, 5, 9, 15, 0, 4. С помощью цикла for и оператора if выведите на экран столбец тех элементов массива, которые больше 3-х, но меньше 10.
+let number = [2,5,9,15,0,4]
+for(let i = 0; i < number.length; i++) {
+    if(number[i] > 3 && number[i] < 10) {
+        console.log(number[i])
+    }
+}
+//Дан массив с числами. Числа могут быть положительными и отрицательными. Найдите сумму положительных элементов массива.
+let number1 = [2,3,4,5,6,8]
+for(let i = 0; i <= number1.length; i++) {
+    if(number1[i] % 2 === 0) {
+        number1 += i
+        console.log(number1[i])
+    }
+}
+//Дан массив с элементами 1, 2, 5, 9, 4, 13, 4, 10. С помощью цикла for и оператора if проверьте есть ли в массиве элемент со значением, равным 4. Если есть - выведите на экран 'Есть!' и выйдите из цикла. Если нет - ничего делать не надо.
+let num = [1,2,5,9,4,13,4,10]
+for(let i = 0; i < num.length; i++) {
+    if(num[i] === 4) {
+        console.log('Есть')
+    }
+}
+//Дан массив числами, например: [10, 20, 30, 50, 235, 3000]. Выведите на экран только те числа из массива, которые начинаются на цифру 1, 2 или 5.
+let c = [10,20,30,50,235,300] 
+for (let i = 0; i < c.length; i++) {
+    if(c[i] === 1 || c[i] === 2 || c[i] === 5) {
+        console.log('Есть');
+    }
+}
